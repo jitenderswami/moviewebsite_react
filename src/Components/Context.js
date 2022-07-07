a890cd3f2d1a8f98681397ec3bda7b7e50412e9e
Jitender Ks <jitenderswami68017@gmail.com>
2022-07-07 16:35:48 +0530

    Restructured

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -1,70 +1,21 @@
-import React, { useContext, useEffect, useState, useRef, useCallback} from "react";
-import { SEARCH_URL,MOVIE_URL } from "../util/API";
+import React, { useContext, useState, useRef, useCallback} from "react";
+import useFetch from "./useFetch";
+import useInfScroll from "./useInfScroll";
+
 
 const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
 
-    const [movies,setMovies] = useState([])
-    const [query,setQuery] = useState("")
     const [movieShow, setMovieShow] = useState(null)
-    const [isLoading,setisLoading] =useState(true)
-    const [pageNumber,setpageNumber] = useState(1)
+    const[query,setQuery] = useState("")
+    const[pageNumber,setpageNumber] = useState(1)
+    const [isLoading,setisLoading] = useState(true)
 
-    const observer = useRef()
-    const lastMovieCardRef = useCallback(node =>{
-        console.log("returned");
-        if(isLoading) return
-
-        if(observer.current) observer.current.disconnect()
-
-        observer.current = new IntersectionObserver(entries => {
-            if(entries[0].isIntersecting){
-                setpageNumber(prevPage => prevPage + 1)
-                console.log(entries[0].target)
-            }
-
-        },{
-            threshold : 1
-        })
-
-
-        if(node) {
-            observer.current.observe(node)}
-
-    },[isLoading])
-
-    useEffect(() => {
-        setisLoading(true)
-        let timer = setTimeout(() => {
-            fetchMovies();
-            setisLoading(false)
-        }, 800);
-
-
-        return () => clearTimeout(timer);
+    const {movies} = useFetch({query,pageNumber,setisLoading})
+    const {lastMovieCardRef} = useInfScroll({setpageNumber})
     
-    },[query,pageNumber])
 
-
-    const fetchMovies = async () => {
-
-        let url = `${MOVIE_URL}${pageNumber}`
-        
-        if(query){
-            url = `${SEARCH_URL}&page=${pageNumber}&query=${query}`
-        }
-        const res = await fetch(url)
-        const data = await res.json()
-
-        if(pageNumber===1){
-            setMovies(data.results)
-        } else{
-            setMovies(prevData => [...prevData, ...data.results])
-        }
-        console.log(movies.length)
-    }
-
-    return (<AppContext.Provider value={{ movieShow, setMovieShow, movies, query, setQuery, isLoading, lastMovieCardRef, setpageNumber}}>
+    return (<AppContext.Provider value={{ movieShow, setMovieShow, movies, query, setQuery, isLoading,setpageNumber, lastMovieCardRef}}>
             {children}
         </AppContext.Provider>);
 }
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,20 +1,36 @@
+import React from "react";
+import { Link} from 'react-router-dom'
 import { useGlobalContext } from "./Context";
-import MovieSkeleton from './MovieSkeleton';
+import { BACK_POSTER_PATH } from '../util/API';
+import votecolor from '../util/votecolor';
 
 
 const Movie = ({movie,index}) => {
 
-    const {movies,lastMovieCardRef} = useGlobalContext()
+    const {movies,setMovieShow,lastMovieCardRef} = useGlobalContext()
+    const{id,title,vote_average,poster_path,vote_count} = movie;
+    
+    const path = BACK_POSTER_PATH + poster_path;
+
+    let reference
+
+    {movies.length === index+1 ? (reference = lastMovieCardRef) : (reference = null)}
 
 
     return(
-        <>
-        {movies.length === index+1 ? (
-            <MovieSkeleton ref={lastMovieCardRef} movie={movie} />
-        ): (
-            <MovieSkeleton movie={movie}/>
-         )}
-        </>
+        <div ref={reference} className="movie">
+            <img src={path} alt={title} />
+            <div className="movie_info">
+                <div className="movietitle">
+                    <h3>{title}</h3>
+                </div>
+                <div className="rating">
+                    <span className={votecolor(vote_average)}>{vote_average}</span>
+                    <span>Votes:{" "}{vote_count}</span>
+                </div>
+                    <Link to= {`/movie/${id}`} className="more_detail"><p onClick={()=>setMovieShow(movie)}>More details</p> </Link>
+            </div>
+        </div>
     )
   
 }
